08bb8aa47c49147c59f011d6a77caf38a97495a5
Ruslan <110040060+Deadusik@users.noreply.github.com>
2023-11-15 06:34:08 +0200

    add redux
    create several reducers
    combined them

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
+  const dispatch = useDispatch()
+  const cash = useSelector(state => state.cash.cash)
+  console.log(cash)
+
+  const addCash = (cash) => {
+    dispatch({ type: 'ADD_CASH', payload: cash })
+  }
+
+  const removeCash = (cash) => {
+    dispatch({ type: 'GET_CASH', payload: cash })
+  }
+
   return (
     <div className="App">
-
+      <div>
+        <p>{cash}</p>
+        <button onClick={() => addCash(Number(prompt('How much cash do you want to add?')))}>Add money</button>
+        <button onClick={() => removeCash(Number(prompt('How much cash do you want to remove?')))}>Remove money</button>
+      </div>
     </div>
   );
 }
